34138b0e7875ba80a3c5afe87bd6516fcd89944c
boneinegg <316786195@qq.com>
2018-08-04 18:28:35 +0800

    algs4 commit_1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.printf("%14.5s", "Hello World!");
+    }
+}
---
+++ src/chapter_1/Sol02.java
@@ -0,0 +1,8 @@
+package chapter_1;
+
+public class Sol02 {
+    public static void main(String[] args) {
+        Object d = (Object)((1 + 2.236)/2);
+        System.out.println(d.getClass().toString());
+    }
+}
